1cc37088eb94d79ca7b7f6f4761dca97ebf974c1
dungpd <dungdzaj255@gmail.com>
2025-08-30 15:24:58 +0700

    update App

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,20 +1,22 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import HeaderBar from "./components/HeaderBar";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
-
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
-
     return (
-        <BrowserRouter>
-            <HeaderBar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <HeaderBar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
+
